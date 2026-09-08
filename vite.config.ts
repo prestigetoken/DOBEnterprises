4ebb6eedac2d50498bb657a561af7c30b42bf810
BlackKnife <42945829+prestigetoken@users.noreply.github.com>
2026-09-08 14:42:55 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/DOBEnterprises/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
